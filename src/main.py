dfbd919f9a8f6f67df373d2f605d78cb68114c71
atakanbozkurt <aa97bozkurt@gmail.com>
2020-03-22 14:51:36 -0400

    Query processing done, need you to check outputs. Need to find correct postings list for given query term.

--- src/main.py
+++ src/main.py
@@ -1,31 +1,40 @@
 import os.path
 from WeightFunctions import GetTfWeights, NormalizeDocLength
 from DictionaryFunctions import BuildDictionary
+from QueryProcessing import TokenizeQuery
+
 
 def main():
+    query = "TI shares decline in late trading; H-P rises 914 479 73 60"
 
     #1)Read dictionary.txt and use hash table to implement a dictionary
     dictionary = BuildDictionary()
+    #2) Read postings.txt" to compute term frequency weight
+    tf_weights = GetTfWeights()
+    #3) Sum term weights used in a document and normalize the length
+    normalized_docs = NormalizeDocLength(tf_weights)
+    #4) Parse input queries
+    tokens = TokenizeQuery(query)
+    #5) Find the correct postings list for any query term
+    #~~~~
 
+    ''' 
     print("-------------   Dictionary    --------------------")
     for e in dictionary:
         print(e,"--> ", dictionary[e])
-    
-
-    #2) Read postings.txt" to compute term frequency weight
-    tf_weights = GetTfWeights()
 
     print("\n\n-------------   Tf_Weights    --------------------")
     for e in tf_weights:
         print(e)
     
-    
-    #3) Sum term weights used in a document and normalize the length
-    normalized_docs = NormalizeDocLength(tf_weights)
-    
     print("\n\n-------------   Normalized doc length   --------------------")
     for n in normalized_docs:
         print(n)
+    '''
+    print("-------------   Tokenization   --------------------")
+    print("Query: ", query , "\nTokens: " , tokens)
+
+
 
     return
 
